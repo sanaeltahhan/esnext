f03a733b017ab7b727368b81afb3768777bda5da
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-27 14:03:10 +0200

    TP syntaxe esnext+ let et const

---
+++ app.js
@@ -0,0 +1,15 @@
+// LET
+let favoriteCityId = "rome";
+console.log(favoriteCityId);
+
+favoriteCityId= "paris";
+console.log(favoriteCityId);
+
+// CONST
+const citiesId = ["paris", "nyc", "rome", "rio-de-janeiro", "tokyo"];
+console.log(citiesId);
+
+//citiesId = [];
+ //citiesId = ["paris", "nyc", "rome", "rio-de-janeiro","tokyo"];
+
+console.log(citiesId);
